af92f285c9793ebf7e9132d20cf2c27eb0e29910
Soham Kulkarni <sohamkrox@gmail.com>
2022-08-13 22:34:41 -0500

    Add cors

--- index.ts
+++ index.ts
@@ -2,11 +2,17 @@ import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import eventsRouter from './src/routes/events';
+import cors from 'cors';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
 const mongoConnectOptions: mongoose.ConnectOptions = {
   keepAlive: true,
