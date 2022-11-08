0f185b90d41821e6077a09c74a6e7bd17c1d7e11
Soham Kulkarni <sohamkrox@gmail.com>
2022-11-08 12:31:20 -0600

    Add Discord OAuth2 authentication

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,15 +1,73 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import session from 'express-session';
+import passport from 'passport';
+import passportDiscord from 'passport-discord';
+dotenv.config();
 const authRouter = express.Router();
 
-// middleware that is specific to this router
-authRouter.use((_req, _res, next) => {
-  console.log('Time: ', Date.now(), '/auth' + _req.url);
-  next();
+var scopes = ['identify'];
+var prompt = 'consent';
+
+console.log('client id', process.env.DISCORD_CLIENT_ID);
+
+const DiscordStrategy = new passportDiscord.Strategy(
+  {
+    clientID: process.env.DISCORD_CLIENT_ID || '',
+    clientSecret: process.env.DISCORD_CLIENT_SECRET || '',
+    callbackURL: 'http://localhost:7000/auth/callback',
+    scope: scopes,
+  },
+  function (_accessToken, _refreshToken, profile, done) {
+    process.nextTick(function () {
+      return done(null, profile);
+    });
+  }
+);
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((obj: any, done) => done(null, obj));
+passport.use(DiscordStrategy);
+
+authRouter.use(
+  session({
+    secret: process.env.SESSION_SECRET || '',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+authRouter.use(passport.initialize());
+authRouter.use(passport.session());
+
+authRouter.get(
+  '/',
+  passport.authenticate('discord', { scope: scopes, prompt: prompt }),
+  () => {}
+);
+
+authRouter.get(
+  '/callback',
+  passport.authenticate('discord', { failureRedirect: '/' }),
+  function (_req, res) {
+    res.redirect('/auth/info');
+  } // auth success
+);
+
+authRouter.get('/logout', function (req, res) {
+  req.logout({}, () => {});
+  res.redirect('/');
 });
 
-// define the home page route
-authRouter.get('/', async (_req, _res) => {
-  _res.send('hello');
-});
+authRouter.get(
+  '/info',
+  (req, res, next) => {
+    if (req.isAuthenticated()) return next();
+    res.send('not logged in :(');
+  },
+  function (req, res) {
+    //console.log(req.user)
+    res.json(req.user);
+  }
+);
 
 export default authRouter;
